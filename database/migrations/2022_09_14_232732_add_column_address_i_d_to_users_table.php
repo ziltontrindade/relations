58e77edc51863a90f7758946fd773b2d47c950c1
Zilton Cordeiro <ztc941@gmail.com>
2022-09-14 20:39:13 -0300

    Criado o relacionamento a nivel de banco

--- database/migrations/2022_09_14_232732_add_column_address_i_d_to_users_table.php
+++ database/migrations/2022_09_14_232732_add_column_address_i_d_to_users_table.php
@@ -13,15 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('address_id')->nullable();
         });
     }
 
@@ -32,6 +25,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('address_id');
+        });
     }
 };
--- database/migrations/2022_09_14_232930_create_foreignkey_users_addresses.php
+++ database/migrations/2022_09_14_232930_create_foreignkey_users_addresses.php
@@ -13,15 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
 
+            $table->foreign('address_id')->references('id')->on('addresses');
         });
     }
 
@@ -32,6 +26,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 };
