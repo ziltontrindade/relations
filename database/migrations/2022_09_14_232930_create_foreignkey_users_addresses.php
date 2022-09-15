95b44b8cb74bc351d300ff792b8025b634ae4fe6
Zilton Cordeiro <ztc941@gmail.com>
2022-09-15 12:20:08 -0300

    Seed Criada para add users and address

--- database/migrations/2022_09_14_232930_create_foreignkey_users_addresses.php
+++ database/migrations/2022_09_14_232930_create_foreignkey_users_addresses.php
@@ -15,7 +15,10 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
 
-            $table->foreign('address_id')->references('id')->on('addresses');
+            $table->foreign('address_id')
+            ->references('id')
+            ->on('addresses')
+            ->onDelete('SET NULL');
         });
     }
 
@@ -27,7 +30,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropForeign('address_id');
         });
     }
 };
---
+++ database/seeders/CreateUserAndAddressSeed.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class CreateUserAndAddressSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            "name" => "Zilton",
+	        "email"=> "zilton@hotmail.com",
+	        "password"=> Hash::make("123456"),
+        ]);
+
+        DB::table('addresses')->insert([
+            "address" => "Rua das treta, nº 666"
+	     
+	       
+        ]);
+    }
+}
